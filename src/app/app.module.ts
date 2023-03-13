e0763fc9a742bf70861f72eb6b8cf5cf0a1070fa
Lila Ami <chuatam80@gmail.com>
2023-03-13 23:21:00 +0700

    add table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TimeComponent } from '../time/time.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [		
+    AppComponent,
+    TimeComponent,
+   ],
   imports: [
     BrowserModule
   ],
